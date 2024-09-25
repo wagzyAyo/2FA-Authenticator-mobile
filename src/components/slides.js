e037307d0809d18c990b7bfdc3e6024b4382424b
Ayo <talktojmcvibes@gmail.com>
2024-09-25 11:55:34 +0100

    create slides for onboarding

---
+++ src/components/slides.js
@@ -0,0 +1,21 @@
+const slides =[
+    {
+        key: 1,
+        title: 'Stronger security',
+        text: 'Enhance your 2FA app with time-based one-time passwords (TOTP) and secure QR code encryption for stronger, reliable authentication. ',
+        image: require('../../assets/board1.png'),
+      },
+    {
+        key: 2,
+        title: 'Unique code to sign in',
+        text: 'Sign in securely with a unique time-based one-time password (TOTP) that refreshes every 30 seconds, ensuring dynamic protection for each login attempt.',
+        image: require('../../assets/board2.png'),
+      },
+    {
+        key: 3,
+        title: 'Simple setup using camera',
+        text: 'Easily set up your 2FA by scanning a secure QR code with your phone’s camera, linking your account in seconds',
+        image: require('../../assets/board3.png'),
+      },
+    
+]
