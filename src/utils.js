9dc3f81126eafeed03371ed87b2f434291615734
Ayo <talktojmcvibes@gmail.com>
2024-09-30 06:54:10 +0100

    create styles utils

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import { Dimensions } from "react-native";
+
+const {width , height } = Dimensions.screen;
+
+
+export const COLORS = {
+    primary: "#0AADAD",
+    secondary: "#3096BD",
+    background: "#FFFFFF",
+    surface: "#000000"
+}
+
+export const typo = {
+    h1: 33,
+    h2: 23,
+    h3: 19,
+    bodyLarge: 16,
+    bodySmall: 14,
+    button: 14,
+    caption: 12,
+}
