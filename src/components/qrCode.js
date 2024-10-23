e4806092b9b9e6e2a016b3755f34b94e579f7f94
Ayo <talktojmcvibes@gmail.com>
2024-10-23 20:56:01 +0100

    qrscanner

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -1,31 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Alert } from 'react-native';
-import { BarCodeScanner } from 'expo-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function QrScanner() {
-  const [hasPermission, setHasPermission] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    Alert.alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+export default function QrScanner({navigation}) {
+  
 
   return (
     <BarCodeScanner
-      onBarCodeScanned={handleBarCodeScanned}
+      
       style={{ flex: 1 }}
     >
       <View>
