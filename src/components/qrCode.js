ee93f3afe29a7c994ad5c6685af4a8d38bc0c6de
Ayo <talktojmcvibes@gmail.com>
2024-10-24 13:25:24 +0100

    implement Qrcode on android: back camera

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -1,40 +1,22 @@
 import React, { useState, useContext } from 'react';
 import { View, Text, Alert, Pressable, StyleSheet } from 'react-native';
-import { useCameraPermissions } from 'expo-camera';
-import { Link } from 'expo-router';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { CameraView} from 'expo-camera';
 import { COLORS, SIZES } from "../styles";
 import { ThemeContext } from '../components/Theme';
 
 export default function QrScanner({ navigation }) {
   const {themeMode} = useContext(ThemeContext);
   const styles = getStyles(themeMode)
-  const [permission, requestPermission] = useCameraPermissions();
-  const isPermissionGranted = Boolean(permission?.granted);
-
+  
   return (
     <View style={styles.container}>
-       <MaterialIcons 
-                name={'arrow-back'}
-                size={24}
-                onPress={()=>(navigation.navigate('Home'))}
-                color={themeMode ? COLORS.background : COLORS.surface}
-                style={[styles.icon, {marginTop: 50}]}
-                />
-      <View style={styles.req}>
-      <Pressable onPress={requestPermission} style={styles.btn}>
-        <Text style={styles.btnText}>Request permission</Text>
-      </Pressable>
-
-      
-          <View style={styles.spacing}>
-            <Link href={'/scanner'} asChild>
-              <Pressable disabled={!isPermissionGranted} style={styles.btn}>
-                <Text style={styles.btnText}>Scan Code</Text>
-              </Pressable>
-            </Link>
-          </View>
-        </View>
+      <CameraView 
+        style={StyleSheet.absoluteFillObject}
+        facing='back'
+        onBarcodeScanned={({data})=>{
+          console.log(data)
+        }}
+      />
     </View>
   );
 }
@@ -45,24 +27,5 @@ const getStyles = (themeMode)=> StyleSheet.create({
     backgroundColor: themeMode ? COLORS.surface : COLORS.background,
     padding: 16,
     
-  },
-  req: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btn: {
-    backgroundColor: COLORS.secondary,
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    marginVertical: 8, // Additional vertical margin for buttons
-  },
-  btnText: {
-    color: COLORS.surface,
-    fontSize: 16,
-  },
-  spacing: {
-    marginTop: 32, // Space between the buttons
   },
 });
