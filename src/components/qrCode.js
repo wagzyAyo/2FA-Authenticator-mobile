ecf959d8bd73db8e39067f22da5fc11228df6bfb
Ayo <talktojmcvibes@gmail.com>
2024-10-24 12:24:09 +0100

    update qrCode screen UI

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -1,24 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Alert, Pressable } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, Alert, Pressable, StyleSheet } from 'react-native';
 import { useCameraPermissions } from 'expo-camera';
 import { Link } from 'expo-router';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { COLORS, SIZES } from "../styles";
+import { ThemeContext } from '../components/Theme';
 
-export default function QrScanner({navigation}) {
+export default function QrScanner({ navigation }) {
+  const {themeMode} = useContext(ThemeContext);
+  const styles = getStyles(themeMode)
   const [permission, requestPermission] = useCameraPermissions();
   const isPermissionGranted = Boolean(permission?.granted);
 
   return (
-    <View>
-      <Pressable onPress={requestPermission}>
-        <Text>Request permission</Text>
+    <View style={styles.container}>
+       <MaterialIcons 
+                name={'arrow-back'}
+                size={24}
+                onPress={()=>(navigation.navigate('Home'))}
+                color={themeMode ? COLORS.background : COLORS.surface}
+                style={[styles.icon, {marginTop: 50}]}
+                />
+      <View style={styles.req}>
+      <Pressable onPress={requestPermission} style={styles.btn}>
+        <Text style={styles.btnText}>Request permission</Text>
       </Pressable>
-      <Link href={'/scanner'} asChild>
-          <Pressable disabled={!isPermissionGranted}>
-             <Text>
-                Scan Code
-             </Text>
-          </Pressable>
-      </Link>
+
+      
+          <View style={styles.spacing}>
+            <Link href={'/scanner'} asChild>
+              <Pressable disabled={!isPermissionGranted} style={styles.btn}>
+                <Text style={styles.btnText}>Scan Code</Text>
+              </Pressable>
+            </Link>
+          </View>
+        </View>
     </View>
   );
 }
+
+const getStyles = (themeMode)=> StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themeMode ? COLORS.surface : COLORS.background,
+    padding: 16,
+    
+  },
+  req: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: COLORS.secondary,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginVertical: 8, // Additional vertical margin for buttons
+  },
+  btnText: {
+    color: COLORS.surface,
+    fontSize: 16,
+  },
+  spacing: {
+    marginTop: 32, // Space between the buttons
+  },
+});
